d4a997e8b2b9e4c5e87107934f68544ca02ae520
WilliamNyamu <williamnyamu08@gmail.com>
2025-08-14 21:23:28 +0300

    More Bank Updates

--- my-tutorial/hmwk/bank.py
+++ my-tutorial/hmwk/bank.py
@@ -13,7 +13,7 @@ class BankAccount:
         if 0 < amount < self.__account_balance:
             self.__account_balance -= amount
             return True
-        return False
+        return False # The pythonic way. Use this instead of nesting it with 'else'
 
     def display_balance(self):
         print(f"Current balance: ${self.__account_balance}")
--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,18 @@
 # bank_account.py
 
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance  # Encapsulated attribute
-
+    def __init__ (self, initial_balance = 0):
+        self.__account_balance = initial_balance
+    
     def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
 
     def withdraw(self, amount):
-        if 0 < amount <= self.__account_balance:
+        if 0 < amount < self.__account_balance:
             self.__account_balance -= amount
             return True
-        return False
+        return False # The pythonic way. Use this instead of nesting it with 'else'
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance}")
+        print(f"Current balance: ${self.__account_balance}")
